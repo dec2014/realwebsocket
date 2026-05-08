5b917707e5be3842d111f6040f174198a181b67e
Vishesh Jain <visheshjain@Visheshs-MacBook-Air.local>
2026-05-08 18:06:12 +0530

    fixed websocket protocol

--- jatte/asgi.py
+++ jatte/asgi.py
@@ -1,13 +1,21 @@
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import URLRouter,ProtocolTypeRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-from chat import routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jatte.settings')
 
+
+
+from django.core.asgi import get_asgi_application
+
 django_asgi_application = get_asgi_application()
+
+
+from chat import routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter,ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+
 
 application=ProtocolTypeRouter({
     'http':django_asgi_application,
